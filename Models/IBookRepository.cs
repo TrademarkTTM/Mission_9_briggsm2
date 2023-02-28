fb9fd701d7dab8bffe8c27e745e372cb8513fa44
Mark Briggs <markbriggs321@gmail.com>
2023-02-28 15:55:04 -0700

    Add project files.

---
+++ Models/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_9_briggsm2.Models
+{
+    public interface IBookRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
---
+++ Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_9_briggsm2.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
